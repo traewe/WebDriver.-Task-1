9c442daace47132e68dfe4f12f694763e98a6409
Oleksii Kovalenko <alex59.kovalenko@gmail.com>
2024-04-29 23:33:44 +0300

    Add project files.

---
+++ WebDriver. Task 1/PastebinPage.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace PastebinTest
+{
+    public class PastebinPage
+    {
+        private IWebDriver driver;
+
+        public PastebinPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://pastebin.com");
+        }
+
+        public void CreateNewPaste(string code, string title)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            var element = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("postform-text")));
+
+            // Setting paste text
+            element.SendKeys(code);
+
+            var expirationDropdown = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("select2-postform-expiration-container")));
+            expirationDropdown.Click();
+
+            // Setting 10 minutes paste expiration
+            var option = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[text()='10 Minutes']")));
+            option.Click();
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].setAttribute('aria-activedescendant', 'select2-postform-expiration-result-fjlh-10M')", expirationDropdown);
+
+            // Setting paste name
+            var titleInput = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("postform-name")));
+            titleInput.SendKeys(title);
+
+            var createButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button.btn.-big[type='submit']")));
+            createButton.Click();
+        }
+
+
+        public bool IsPasteCreatedSuccessfully()
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            var textarea = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("de1")));
+
+            return textarea.Text == "Hello from WebDriver";
+        }
+    }
+}
---
+++ WebDriver. Task 1/PastebinTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PastebinTest
+{
+    [TestFixture]
+    public class PastebinTest
+    {
+        private IWebDriver driver;
+        private PastebinPage pastebinPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            pastebinPage = new PastebinPage(driver);
+        }
+
+        [Test]
+        public void CreateNewPaste()
+        {
+            pastebinPage.Open();
+
+            pastebinPage.CreateNewPaste("Hello from WebDriver", "helloweb");
+
+            Assert.That(pastebinPage.IsPasteCreatedSuccessfully(), Is.EqualTo(true));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
